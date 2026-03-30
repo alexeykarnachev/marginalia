64e227a452e5e657c76dc1e1defc5f1d728e713f
Alexey Karnachev <alekseykarnachev@gmail.com>
2026-03-30 21:52:51 +0300

    fix

--- svelte-app/public/sw.js
+++ svelte-app/public/sw.js
@@ -1,17 +1,12 @@
 const MARGINALIA_VERSION = 167;
 const CACHE_NAME = "marginalia-v" + MARGINALIA_VERSION;
 
-self.addEventListener("install", (e) => {
-    self.skipWaiting();
-});
-
 self.addEventListener("activate", (e) => {
     e.waitUntil(
         caches.keys().then((keys) =>
             Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
         )
     );
-    self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
